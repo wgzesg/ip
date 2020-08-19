bc8574a763592cc0324d6f732e8882a99df3d9d3
zesong <wgzesg@gmail.com>
2020-08-19 10:50:29 +0800

    Added greeting and bye print

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,5 +6,24 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        printSeperationLine();
+        printGreetings();
+        printBye();
+    }
+
+    static void printGreetings(){
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
+        printSeperationLine();
+    }
+
+    static void printSeperationLine(){
+        System.out.println("------------------------------------------");
+    }
+
+    static void printBye(){
+        System.out.println(" Bye. Hope to see you again soon!");
+        printSeperationLine();
     }
 }
