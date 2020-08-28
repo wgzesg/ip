d0797b2a40b004f5d0fd146c6c17b9bab86eb9aa
wgzesg <wgzesg@gmail.com>
2020-08-28 23:30:37 +0800

    Updated Level 3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -19,40 +19,50 @@ public class Duke {
         Scanner in = new Scanner(System.in);
         while(true){
             String command = in.nextLine();
-
-            processCommand(command);
+            int result = processCommand(command);
+            if(result == -1){
+                break;
+            }
         }
+
     }
 
-    static void processCommand(String command){
+    static int processCommand(String command){
+        int result = 0;
         printSeperationLine();
         String[] args = command.split(" ");
         if(args[0].equals(("done"))){
-            try{
-                int index = Integer.parseInt(args[1]);
-                todolist.get(index - 1).markAsDone();
-                System.out.println("Nice! I've marked this task as done: ");
-                System.out.println(todolist.get(index - 1).printTask());
-            } catch(NumberFormatException e) {
-                System.out.println("The command is not recognised. Do you wish to add this as task? (y/n)");
-                Scanner in = new Scanner(System.in);
-                String response = in.next();
-                if (response.equals("yes") || response.equals("Yes") || response.equals("y")) {
-                    addToTodoList(command.trim());
-                } else {
-                    System.out.println("This action is aborted.");
-                }
-            } catch (IndexOutOfBoundsException e){
-                System.out.println("The index is out of range. This action is aborted.");
-            }
+            markDone(command, args[1]);
         } else if (command.equals("list")){
             listAll();
         } else if(command.equals("bye")){
             printBye();
+            result = -1;
         } else{
             addToTodoList(command.trim());
         }
         printSeperationLine();
+        return result;
+    }
+
+    private static void markDone(String command, String arg) {
+        try{
+            int index = Integer.parseInt(arg);
+            todolist.get(index - 1).markAsDone();
+            System.out.println("Nice! I've marked this task as done: ");
+            System.out.println(todolist.get(index - 1).printTask());
+        } catch(NumberFormatException e) {
+            System.out.println("The command is not recognised. Do you wish to add this as task? (y/n)");
+            Scanner in = new Scanner(System.in);
+            String response = in.next();
+            if (response.equals("yes") || response.equals("Yes") || response.equals("y")) {
+                addToTodoList(command.trim());
+            } else {
+                System.out.println("This action is aborted.");
+            }
+        } catch (IndexOutOfBoundsException e){
+            System.out.println("The index is out of range. This action is aborted.");
+        }
     }
 
     static void listAll(){
@@ -73,6 +83,7 @@ public class Duke {
         System.out.println(input);
         printSeperationLine();
     }
+
     static void printGreetings(){
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
@@ -85,6 +96,5 @@ public class Duke {
 
     static void printBye(){
         System.out.println("Bye. Hope to see you again soon!");
-        printSeperationLine();
     }
 }
